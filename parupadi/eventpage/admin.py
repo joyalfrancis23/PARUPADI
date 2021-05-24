bfd7bbc0209dbb8600a2ae8b07f17d691ee7ec39
joyalfrancis23 <joyalfrancis23@gmail.com>
2021-05-24 21:19:14 +0530

    new event creation done

--- parupadi/eventpage/admin.py
+++ parupadi/eventpage/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Eventpage
 class EventpageAdmin(admin.ModelAdmin):
-    list_display = ('Event_name','Date_Time','Event_location','max_participants')
+    list_display = ('Event_name','Date','Time','Event_location','max_participants')
 # Register your models here.
 admin.site.register(Eventpage,EventpageAdmin)
--- parupadi/eventpage/views.py
+++ parupadi/eventpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Eventpage
+from datetime import datetime
 
 # Create your views here.
 def home(request):
@@ -10,4 +11,23 @@ def registersuccess(request):
     return render(request,'registersuccess.html')
 
 def eventregistration(request):
-    return render(request,'eventregistration.html');
+    if request.method == 'POST':
+        title = request.POST.get('eventname')
+        date = request.POST.get('eventdate')
+        time = request.POST.get('eventtime')
+        location = request.POST.get('eventlocation')
+        description = request.POST.get('eventdescription')
+        max_participants = request.POST.get('maxparticipants')
+        image_url = request.POST.get('imageurl')
+        print(title,date,time,location,description,max_participants,image_url)
+        obj = Eventpage()
+        obj.Event_name = title
+        obj.Date = date
+        obj.Time = time
+        obj.Event_location = location
+        obj.Event_Description = description
+        obj.max_participants = max_participants
+        obj.image = image_url
+        obj.save()
+
+    return render(request,'eventregistration.html')
