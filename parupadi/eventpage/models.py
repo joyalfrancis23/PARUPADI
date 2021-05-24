3e7fb4b4d849d448744054381a8462f8eee851a0
joyalfrancis23 <joyalfrancis23@gmail.com>
2021-05-24 21:58:16 +0530

    event registration done

--- parupadi/eventpage/models.py
+++ parupadi/eventpage/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime
+
+from django.db.models.fields import EmailField
 # Create your models here.
 class Eventpage(models.Model):
     Event_name=models.CharField(max_length=255)
@@ -9,3 +11,10 @@ class Eventpage(models.Model):
     Event_Description=models.CharField(max_length=500)
     max_participants=models.IntegerField()
     image=models.CharField(max_length=25000)
+
+class Participants(models.Model):
+    Name = models.CharField(max_length=255)
+    Branch = models.CharField(max_length=255)
+    Semester = models.IntegerField()
+    Phone = models.IntegerField()
+    Email = models.EmailField()
