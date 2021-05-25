818ec21bbb0d60c64d96d199179cb19170a1e0a4
Joyal Francis TJ <joyalfrancis23@gmail.com>
2021-05-25 17:22:14 +0530

    Update admin.py
    model error fixed

--- eventpage/admin.py
+++ eventpage/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Eventpage,Participants
 class EventpageAdmin(admin.ModelAdmin):
     list_display = ('Event_name','Date','Time','Event_location','max_participants')
+  
+class ParticipantsAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Branch', 'Semester', 'Phone', 'Email')
 
 
 # Register your models here.
 admin.site.register(Eventpage,EventpageAdmin)
+admin.site.register(Participants,ParticipantsAdmin)
